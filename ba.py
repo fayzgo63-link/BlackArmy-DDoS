1a2259ad281186a6919955947945d28ab86bb30d
fayzgo63-link <fayzgo63@gmail.com>
2026-01-17 01:50:10 +0700

    Update ba.py

--- ba.py
+++ ba.py
@@ -75,7 +75,7 @@ class httpth1(threading.Thread):
                 randomized_url = url + "?" + genstr(random.randint(3, 10))
                 requests.get(randomized_url, headers=headers)
                 u += 1
-                print("\033[48;5;1m\033[37m" +str(u)+ " \033[0m\033[36mrequests: \033[33m" +(randomized_url)+ "\033[0m") 
+                print("\033[48;5;1m\033[37m" +str(u)+ " \033[0m\033[36mrequests: \033[33m" +(url)+ "\033[0m") 
             except requests.exceptions.ConnectionError:
                 print("\033[38;5;6m" +(url)+ "\033[0m \033[31mrequests error...!\033[0m")
                 pass
